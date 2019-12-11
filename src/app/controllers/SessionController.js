431045835a2fbbca8c0b71ae498f9ff9f21524ec
Allan Camargo <allan.cdev@gmail.com>
2019-12-11 11:57:15 -0300

    add project in github

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.json(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, 'cdd4c3f64cc6b9570d9f19bc5006be15', {}),
+    });
+  }
+}
+
+export default new SessionController();
