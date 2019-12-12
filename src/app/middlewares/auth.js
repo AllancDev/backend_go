01ced49f9ea3ec4f2a8605d8c4db85f2c88e81d3
Allan Camargo <allan.cdev@gmail.com>
2019-12-12 10:34:04 -0300

    config multer

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
@@ -12,7 +13,11 @@ export default (req, res, next) => {
   const [, token] = authHeader.split(' ');
 
   try {
-} catch (err) {
-    return res.status(401).json();
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
   }
 };
